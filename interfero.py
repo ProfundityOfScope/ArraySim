ddfe3101eacecacba0e4170a78e3d9839dbf0ac0
Seth <bruzewskis@gmail.com>
2024-11-21 00:26:55 -0500

    Made classes smarter

--- interfero.py
+++ interfero.py
@@ -14,6 +14,7 @@ from astropy.coordinates import ICRS, AltAz, SkyCoord, EarthLocation, Angle, ITR
 import astropy.units as u
 from scipy.optimize import newton
 import matplotlib.pyplot as plt
+from typing import Union
 
 
 class Array:
@@ -32,85 +33,129 @@ class Array:
                                     diam=dat['Diameter'][i]))
         return cls(instruments=stations)
 
-
-    def to_geocentric_at_time(self, time):
+    def to_geocentric_at_time(self, time: Time):
         xyzt = np.zeros((3, len(self.instruments)))
         for i, instrument in enumerate(self.instruments):
             if isinstance(instrument, Station):
-                xyzt[:, i] = instrument.location.value.tolist()
+                xyzt[:, i] = instrument.to_geocentric()
             elif isinstance(instrument, Satellite):
-                xyzt[:, i] = instrument.get_pos_at_time(time)
+                xyzt[:, i] = instrument.to_geocentric(time)
             else:
                 raise ValueError
         return xyzt
-    
+
     def to_geocentric_now(self):
         return self.to_geocentric_at_time(Time.now())
+
+
+class AngleProperty:
+    def __init__(self, name):
+        self.name = f'_{name}'
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        if not isinstance(value, Angle):
+            value = Angle(value, unit=u.deg)
+        setattr(instance, self.name, value)
+        
+class DateProperty:
+    def __init__(self, name):
+        self.name = f'_{name}'
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
     
-@dataclass
+    def __set__(self, instance, value):
+        if not isinstance(value, Time):
+            value = Time(value)
+        setattr(instance, self.name, value)
+
 class Station:
-    name: str
-    lat: str
-    lon: str
-    elev: float
-    diam: float
     
-    def __post_init__(self):
-        self._lat_ang = Angle(self.lat, unit='deg')
-        self._lon_ang = Angle(self.lon, unit='deg')
-        self.location = EarthLocation(lon=self._lon_ang,
-                                      lat=self._lat_ang,
+    lat = AngleProperty('lat')
+    lon = AngleProperty('lon')
+    
+    def __init__(self, name: str, lat: Union[float, str, Angle, u.Quantity],
+                 lon: Union[float, str, Angle, u.Quantity],
+                 elev: float, diam: float):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+        self.elev = elev
+        self.diam = diam
+        self.location = EarthLocation(lon=self.lon,
+                                      lat=self.lat,
                                       height=self.elev)
-    
-@dataclass
+
+    def to_geocentric(self):
+        return np.array(self.location.value.tolist())
+
+
 class Satellite:
-    name: str
-    sma: float  # Semi-major axis (km)
-    ecc: float  # ecc
-    inc: float  # inc (degrees)
-    raan: float  # Right Ascension of Ascending Node (degrees)
-    aop: float  # Argument of Periapsis (degrees)
-    mae: float  # Mean Anomaly at Epoch (degrees)
-    epoch: Time  # Epoch for the orbital elements
+    inclination = AngleProperty('inclination')
+    ra_asc = AngleProperty('ra_asc')
+    arg_of_peri = AngleProperty('arg_of_peri')
+    mean_anom = AngleProperty('mean_anom')
+    epoch = DateProperty('epoch')
 
-    def get_pos_at_time(self, time: Time):
-        """
-        Compute geocentric position of satellite.
+    def __init__(self, name: str, semi_major: float, eccen: float,
+                 inclination: Union[float, str, Angle, u.Quantity],
+                 ra_asc: Union[float, str, Angle, u.Quantity],
+                 arg_of_peri: Union[float, str, Angle, u.Quantity],
+                 mean_anom: Union[float, str, Angle, u.Quantity],
+                 epoch: Time):
+        self.name = name
+        self.semi_major = semi_major
+        self.eccen = eccen
+        self.inclination = inclination
+        self.ra_asc = ra_asc
+        self.arg_of_peri = arg_of_peri
+        self.mean_anom = mean_anom
+        self.epoch = epoch
+        
+    def true_anomaly(self, time: Time) -> np.ndarray:
 
-        Compute the geocentric (x, y, z) position of the satellite at a
-        given time.
-        """
+        # Validate time
+        time = np.atleast_1d(time)
+            
         # Step 1: Compute mean motion
         mu = 6.6743e-11 * 5.972e24  # Gravitational parameter G * M (m^3/s^2)
-        n = np.sqrt(mu / self.sma**3)  # Mean motion (rad/s)
-
+        n = np.sqrt(mu / self.semi_major**3)  # Mean motion (rad/s)
+    
         # Step 2: Compute mean anomaly at the given time
         delta_t = (time - self.epoch).to_value("sec")  # Time diff in seconds
-        M = np.radians(self.mae) + n * delta_t
+        M = self.mean_anom.rad + n * delta_t
         M = np.mod(M, 2 * np.pi)  # Keep within 0 to 2pi
-
+    
         # Step 3: Solve Kepler's equation for the eccentric anomaly
         def kepler_eq(E):
-            return E - self.ecc * np.sin(E) - M
-
+            return E - self.eccen * np.sin(E) - M
+    
         E = newton(kepler_eq, M)  # Solve for eccentric anomaly (radians)
-
+    
         # Step 4: Compute true anomaly
-        nu = 2 * np.arctan2(np.sqrt(1 + self.ecc) * np.sin(E / 2),
-                            np.sqrt(1 - self.ecc) * np.cos(E / 2))
+        nu = 2 * np.arctan2(np.sqrt(1 + self.eccen) * np.sin(E / 2),
+                            np.sqrt(1 - self.eccen) * np.cos(E / 2))
+        
+        return np.squeeze(nu)
+    
+    def to_geocentric(self, time: Time):
+        nu = self.true_anomaly(time)
 
         # Step 5: Compute distance to the central body
-        r = self.sma * (1 - self.ecc**2) / (1 + self.ecc * np.cos(nu))
+        r = self.semi_major * (1 - self.eccen**2) / (1 + self.eccen * np.cos(nu))
 
         # Step 6: Satellite position in the orbital plane
         x_orb = r * np.cos(nu)
         y_orb = r * np.sin(nu)
-        z_orb = 0  # In orbital plane
+        z_orb = np.full_like(x_orb, 0)  # In orbital plane
 
         # Step 7: Rotate into geocentric equatorial frame
-        i = np.radians(self.inc)
-        Omega = np.radians(self.raan)
-        omega = np.radians(self.aop)
+        i = self.inclination.rad
+        Omega = self.ra_asc.rad
+        omega = self.arg_of_peri.rad
 
         # Rotation matrices
         R_z_Omega = np.array([[np.cos(Omega), -np.sin(Omega), 0],
@@ -133,20 +178,35 @@ class Satellite:
         pos_geocentric = R @ pos_orb
 
         return pos_geocentric 
-    
+
     def get_period(self):
-        T = np.sqrt( self.sma**3 / (6.6743e-11 * 5.972e24/(4*np.pi**2)) )
+        a = self.semi_major
+        T = np.sqrt( a**3 / (6.6743e-11 * 5.972e24/(4*np.pi**2)) )
         return T
     
+    def orbit_geocentric(self, n=50):
+        period = self.get_period()
+        time_array = self.epoch + np.linspace(0, 1, n) * period * u.s
+        return self.to_geocentric(time_array)
+
+    @classmethod
+    def dummy(cls):
+        return cls('Dummy', 7e6, 0.1, 30, 30, 30, 30, Time.now().iso)
+    
+    def __repr__(self):
+        return f'Satellite(name={self.name}, semi_major={self.sma}, epoch={self.epoch})'
+
+
 def xyz_to_lonlat(pos):
     rad = np.sqrt(np.sum(np.square(pos), axis=0))
     lon = np.arctan2(pos[1], pos[0])
     lat = np.arcsin(pos[2] / rad)
     
     return rad, lon, lat
-    
+
+
 if __name__=='__main__':
-    testtime = Time.now() + 1*u.hr
+    testtime = Time.now()
     
     myarray = Array.from_file('hsa.antpos')
     testsat = Satellite('Fermi', 7e6, 0.001, 25.58, 29.29, 131.16, 229,
@@ -154,31 +214,17 @@ if __name__=='__main__':
     myarray.instruments.append(testsat)
     
     sc = SkyCoord('00h02m00s +62d16\'00"', frame=ICRS)
-    sc_v = sc.transform_to(ITRS(obstime=testtime))
-    vec = sc_v.cartesian.xyz.value
     
     sites = myarray.instruments
     pos = myarray.to_geocentric_at_time(testtime) / 6.378e6
     maxr = np.max(np.sqrt(np.sum(np.square(pos), axis=0)))
     
     # Plot satellite orbit circle
-    sattime = testtime + np.linspace(0, 1, 50) * testsat.get_period() * u.s
-    satcircle = np.array([ testsat.get_pos_at_time(t)/6.378e6 for t in sattime ]).T
+    satcircle = testsat.orbit_geocentric()
     
     # Plot sky Circle drawn by celestial object
     skytime = testtime + np.linspace(0, 1, 50) * u.day
-    skycircle = np.array([ sc.transform_to(ITRS(obstime=t)).cartesian.xyz.value for t in skytime ]).T
-    
-    plt.figure(figsize=(10, 10))
-    ax = plt.subplot(projection='3d')
-    ax.scatter(*pos)
-    ax.set_xlim(-maxr, maxr)
-    ax.set_ylim(-maxr, maxr)
-    ax.set_zlim(-maxr, maxr)
-    ax.plot([0, vec[0]], [0, vec[1]], [0, vec[2]])
-    ax.plot(*satcircle)
-    ax.plot(*skycircle)
-    plt.show()
+    skycircle = sc.transform_to(ITRS(obstime=skytime)).cartesian.xyz.value
     
     pr, plon, plat = xyz_to_lonlat(pos)
     sr, slon, slat = xyz_to_lonlat(satcircle)
@@ -187,8 +233,7 @@ if __name__=='__main__':
     plt.figure(figsize=(10, 5), dpi=1920/10)
     plt.subplot(projection='mollweide')
     plt.scatter(plon, plat, label='Ground Site')
-    plt.scatter(slon, slat, c='C1', s=5)
-    plt.scatter(slon[0], slat[0], ec='C1', fc='w', s=100, label='Satellite Position')
+    plt.scatter(slon, slat, c='C1', s=5, label='Satellite Path')
     plt.scatter(elon, elat, c='C2', s=5)
     plt.scatter(elon[0], elat[0], ec='C2', fc='w', s=100, label='Celestial Object Position')
     plt.grid()
